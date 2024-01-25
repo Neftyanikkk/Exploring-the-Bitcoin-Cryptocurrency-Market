860d357f41b973d7f2676ec3f515fde86d204a0a
Neftyanikkk <roman.groshev2017@yandex.ru>
2024-01-26 01:25:32 +0300

    third task

--- main.py
+++ main.py
@@ -21,3 +21,19 @@ cap = market_cap_raw.query('market_cap_usd > 0')
 
 # Counting the number of values again
 cap.count()
+
+#Declaring these now for later use in the plots
+TOP_CAP_TITLE = 'Top 10 market capitalization'
+TOP_CAP_YLABEL = '% of total cap'
+
+# Selecting the first 10 rows and setting the index
+cap10 = cap[:10].set_index('id')
+
+# Calculating market_cap_perc
+cap10 = cap10.assign(market_cap_perc = lambda x: (x.market_cap_usd / cap.market_cap_usd.sum())*100)
+
+# Plotting the barplot with the title defined above 
+ax = cap10.market_cap_perc.plot.bar(title=TOP_CAP_TITLE)
+
+# Annotating the y axis with the label defined above
+ax.set_ylabel(TOP_CAP_YLABEL);
